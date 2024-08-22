ab858cb9bca2abc1627451a87852b0fbb11ee9bc
Ayush Patel <148226375+ayush-patel1@users.noreply.github.com>
2024-08-22 14:31:47 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (57.13%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        int mask = 0;                                
+        for(int tmp = num; tmp; tmp >>= 1)            
+            mask = (mask << 1) | 1;                   
+			
+        return mask ^ num;                            // finally XORing with mask will flip all 
+    }
+};
