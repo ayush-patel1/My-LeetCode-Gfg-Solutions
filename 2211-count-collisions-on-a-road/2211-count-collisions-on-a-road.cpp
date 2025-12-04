5a82009207d85d28ee2c34b8015d34f4f73f63c3
Ayush Patel <148226375+ayush-patel1@users.noreply.github.com>
2025-12-04 19:23:04 +0530

    Time: 3 ms (95.07%), Space: 19.2 MB (64.06%) - LeetHub

---
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int countCollisions(string s) {
+        int n = s.size(), l = 0, r = n - 1;
+        while (l < n && s[l] == 'L') l++;
+        while (r >= 0 && s[r] == 'R') r--;
+        int ans = 0;
+        for (int i = l; i <= r; i++) if (s[i] != 'S') ans++;
+        return ans;
+    }
+};
