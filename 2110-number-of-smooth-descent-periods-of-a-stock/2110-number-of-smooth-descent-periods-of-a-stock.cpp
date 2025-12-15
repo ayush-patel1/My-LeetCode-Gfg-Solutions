75d5719c7e138c5d9502de1b9cd5a6b5784dc812
Ayush Patel <148226375+ayush-patel1@users.noreply.github.com>
2025-12-15 09:35:37 +0530

    Time: 3 ms (41.64%), Space: 111.6 MB (10.78%) - LeetHub

---
+++ 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+#define ll long long
+    long long getDescentPeriods(vector<int>& prices) {
+        ll n = prices.size();
+        ll i = 0;
+        vector<ll> m;
+
+        while (i < n) {
+            ll cnt = 1;
+            ll prev = prices[i];
+            i++;
+            while (i < n && prev - prices[i] == 1) {
+                cnt++;
+                prev = prices[i];
+                i++;
+            }
+            m.push_back(cnt);
+        }
+
+        ll sm = accumulate(m.begin(), m.end(), 0LL);
+        ll ans = n - sm;
+        for (int i = 0; i < m.size(); i++) {
+            ans += 1LL * m[i] * (m[i] + 1) / 2;
+        }
+        return ans;
+    }
+};
