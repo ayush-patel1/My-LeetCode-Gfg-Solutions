78991d3fa182e5bf6f2567b5e714dd26a3861142
Ayush Patel <148226375+ayush-patel1@users.noreply.github.com>
2025-12-31 13:42:20 +0530

    Time: 256 ms (43.98%), Space: 194 MB (49.48%) - LeetHub

---
+++ 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int dx[4] = {-1, 0, 1, 0};
+    int dy[4] = {0, -1, 0, 1};
+
+    bool check(int d, int row, int col, vector<vector<int>>& cells) {
+        vector<vector<int>> vis(row, vector<int>(col, 0));
+        queue<pair<int,int>> q;
+
+        for (int i = 0; i < d; i++) {
+            vis[cells[i][0] - 1][cells[i][1] - 1] = 1;
+        }
+
+        for (int j = 0; j < col; j++) {
+            if (!vis[0][j]) {
+                q.push({0, j});
+                vis[0][j] = 1;
+            }
+        }
+
+        while (!q.empty()) {
+            auto [x, y] = q.front(); q.pop();
+            if (x == row - 1) return true;
+
+            for (int k = 0; k < 4; k++) {
+                int nx = x + dx[k], ny = y + dy[k];
+                if (nx >= 0 && ny >= 0 && nx < row && ny < col && !vis[nx][ny]) {
+                    vis[nx][ny] = 1;
+                    q.push({nx, ny});
+                }
+            }
+        }
+        return false;
+    }
+
+    int latestDayToCross(int row, int col, vector<vector<int>>& cells) {
+        int l = 0, r = row * col, ans = 0;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (check(mid, row, col, cells)) {
+                ans = mid;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return ans;
+    }
+};
