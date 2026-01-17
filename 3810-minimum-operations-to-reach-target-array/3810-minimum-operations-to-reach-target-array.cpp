b25947e4a7c6953be66ddbe7a0c964c0c5ec4413
Ayush Patel <148226375+ayush-patel1@users.noreply.github.com>
2026-01-17 22:39:52 +0530

    Time: 103 ms (51.95%), Space: 257.4 MB (58.14%) - LeetHub

---
+++ 3810-minimum-operations-to-reach-target-array/3810-minimum-operations-to-reach-target-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, vector<int>& target) {
+        int n=nums.size();
+        unordered_map<int,int>mp;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=target[i]){
+                mp[nums[i]]++;
+            }
+        }
+        return mp.size();
+    }
+};
