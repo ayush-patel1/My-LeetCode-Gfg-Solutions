dc5fa2afee0f83f7625d7379a4cbc49e5b47d4e3
Ayush Patel <148226375+ayush-patel1@users.noreply.github.com>
2026-02-01 11:07:49 +0530

    Time: 0 ms (100%), Space: 30.3 MB (96.48%) - LeetHub

--- 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.cpp
+++ 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
     int minimumCost(vector<int>& nums) {
-        
+        int sum=nums[0];  //phela element to include krna hi hoga, uske baad minmum and second minimum ka sum;
+        int min=INT_MAX;
+        int ans=0;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]<min){
+                min=nums[i];
+                ans=i;
+            }
+        } 
+        sum+=min;
+        min=INT_MAX;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]<min && i!=ans){
+                min=nums[i];
+            }
+
+        }
+        sum+=min;
+    return sum;    
     }
 };
