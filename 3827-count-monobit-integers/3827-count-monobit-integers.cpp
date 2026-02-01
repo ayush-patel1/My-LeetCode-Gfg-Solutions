3cc80ff375209021b736ce23951a0efdf61c3f73
Ayush Patel <148226375+ayush-patel1@users.noreply.github.com>
2026-02-01 11:19:05 +0530

    Time: 0 ms (100%), Space: 8.6 MB (25.59%) - LeetHub

--- 3827-count-monobit-integers/3827-count-monobit-integers.cpp
+++ 3827-count-monobit-integers/3827-count-monobit-integers.cpp
@@ -3,19 +3,13 @@ public:
     int countMonobit(int n) {
         int ans = 0;
         for (int i = 0; i <= n; i++) {
-
-            if (i == 0) { 
-                ans++; 
-                continue; 
-            }
-
             int num = i, cnt = 0;
             for (int j = 0; j < 32; j++) {
                 if ((1 << j) & i) {
                     cnt++;
                     num -= (1 << j);
                     if (num == 0) {
-                        if (j + 1 == cnt) {
+                        if (j + 1 == cnt || cnt == 0) {
                             ans++;
                         }
                         break;
@@ -23,6 +17,6 @@ public:
                 }
             }
         }
-        return ans;
+        return ++ans;
     }
 };
