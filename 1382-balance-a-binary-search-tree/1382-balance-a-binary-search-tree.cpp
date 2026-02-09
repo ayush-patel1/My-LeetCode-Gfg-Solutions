1c4c32ca33429d0c3fd461eb53a2228306ecf28a
Ayush Patel <148226375+ayush-patel1@users.noreply.github.com>
2026-02-09 13:15:10 +0530

    Time: 15 ms (51.89%), Space: 65.6 MB (64.31%) - LeetHub

--- 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.cpp
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.cpp
@@ -11,25 +11,25 @@
  */
 class Solution {
 public:
-    void Inorder(TreeNode* root, vector<int>& inorder) {
-        if (!root) return;
-        Inorder(root->left, inorder);
-        inorder.push_back(root->val);
-        Inorder(root->right, inorder);
+    void Inorder(vector<int>& a, TreeNode* r){
+        if(!r) return;
+        Inorder(a,r->left);
+        a.push_back(r->val);
+        Inorder(a,r->right);
     }
-    
-    TreeNode* solve(vector<int>& inorder, int left, int right) {
-        if (left > right) return nullptr;
-        int mid = (left + right) / 2;
-        TreeNode* root = new TreeNode(inorder[mid]);
-        root->left = solve(inorder, left, mid - 1);
-        root->right = solve(inorder, mid + 1, right);
+
+    TreeNode* solve(vector<int>& a,int l,int r){
+        if(l>r) return NULL;
+        int m=(l+r)/2;
+        TreeNode* root=new TreeNode(a[m]);
+        root->left=solve(a,l,m-1);
+        root->right=solve(a,m+1,r);
         return root;
     }
 
     TreeNode* balanceBST(TreeNode* root) {
-        vector<int> inorder;
-        Inorder(root, inorder);
-        return solve(inorder, 0, inorder.size() - 1);
+        vector<int> a;
+        Inorder(a,root);
+        return solve(a,0,a.size()-1);
     }
 };
