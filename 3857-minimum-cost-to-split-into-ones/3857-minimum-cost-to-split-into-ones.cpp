26dff396dbcf4bb4e1dab099bc9815a2a2d05fe0
Ayush Patel <148226375+ayush-patel1@users.noreply.github.com>
2026-03-01 13:09:19 +0530

    Time: 67 ms (5.6%), Space: 11.3 MB (6.76%) - LeetHub

--- 3857-minimum-cost-to-split-into-ones/3857-minimum-cost-to-split-into-ones.cpp
+++ 3857-minimum-cost-to-split-into-ones/3857-minimum-cost-to-split-into-ones.cpp
@@ -1,4 +1,29 @@
 class Solution {
 public:
-    int minCost(int n) { return n * (n - 1) / 2; }
+    using ll = long long;
+    vector<ll> dp;
+
+    ll solve(int n)
+    {
+        if(n <= 1) return 0;
+        if(dp[n] != -1) return dp[n];
+
+        ll cost = 1e18;
+
+        for(int i = 1; i <= n/2; i++)
+        {
+            ll x = i;
+            ll y = n - i;
+
+            ll cur = x*y + solve(x) + solve(y);
+            cost = min(cost, cur);
+        }
+
+        return dp[n] = cost;
+    }
+
+    int minCost(int n) {
+        dp.assign(n + 1, -1);
+        return solve(n);
+    }
 };
