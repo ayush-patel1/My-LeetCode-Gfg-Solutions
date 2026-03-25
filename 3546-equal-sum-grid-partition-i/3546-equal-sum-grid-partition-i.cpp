6c90f99e0551ccca0deb8c8931c4e16f718a3ca7
Ayush Patel <148226375+ayush-patel1@users.noreply.github.com>
2026-03-25 15:30:59 +0530

    Time: 560 ms (5.04%), Space: 298 MB (6.84%) - LeetHub

--- 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.cpp
+++ 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.cpp
@@ -3,28 +3,63 @@ public:
     bool canPartitionGrid(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        long long tot_sum=0;
+
+        long long tot=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++) tot+=grid[i][j];
+        }
+
+        unordered_map<int, vector<pair<int,int>>> mp;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                tot_sum+=grid[i][j];
+                mp[grid[i][j]].push_back({i,j});
             }
         }
-        //check row-wise division
+
         long long sum=0;
+
+        // row cut
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                sum+=grid[i][j];
+            for(int j=0;j<m;j++) sum+=grid[i][j];
+
+            if(i+1<n){
+                if(2*sum==tot) return true;
+
+                long long diff=abs(tot-2*sum);
+
+                if(mp.count(diff)){
+                    for(auto &p:mp[diff]){
+                        int r=p.first,c=p.second;
+                        if(r!=0 && r!=n-1 && c!=0 && c!=m-1){
+                            return true;
+                        }
+                    }
+                }
             }
-            if(2*sum==tot_sum && i+1<n) return true;
         }
-        //check col-wise division
+
         sum=0;
+
+        // col cut
         for(int j=0;j<m;j++){
-            for(int i=0;i<n;i++){
-                sum+=grid[i][j];
+            for(int i=0;i<n;i++) sum+=grid[i][j];
+
+            if(j+1<m){
+                if(2*sum==tot) return true;
+
+                long long diff=abs(tot-2*sum);
+
+                if(mp.count(diff)){
+                    for(auto &p:mp[diff]){
+                        int r=p.first,c=p.second;
+                        if(r!=0 && r!=n-1 && c!=0 && c!=m-1){
+                            return true;
+                        }
+                    }
+                }
             }
-            if(2*sum==tot_sum && j+1<m) return true;
         }
+
         return false;
     }
 };
