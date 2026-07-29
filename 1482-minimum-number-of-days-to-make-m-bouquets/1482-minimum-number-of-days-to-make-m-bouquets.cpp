be97043566589d39ef2611fa074413644e2334da
Ayush Patel <148226375+ayush-patel1@users.noreply.github.com>
2026-07-29 23:22:39 +0530

    Time: 28 ms (21.98%), Space: 70.6 MB (48.53%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
-    bool check(int l, vector<int>& arr, int k, int m) {
-        int cnt = 0;
-        int tot = 0;
-        for (auto& it : arr) {
-            if (it <= l) {
-                cnt++;
-                if (cnt == k) {
-                    cnt = 0;
-                    tot++;
-                }
-            }else cnt=0;
+
+bool check(int mid,vector<int>&v,int k,int m){
+    int b=0;
+    int cnt=0;
+    for(int i=0;i<v.size();i++){
+        if(v[i]<=mid){
+            cnt++;
+            if(cnt>=k){
+                b++;
+                cnt=0;
+            }
+        }else{
+            cnt=0;
         }
-        if (cnt >= k)
-            tot++;
-        return tot >= m;
     }
+    return b>=m;
+}
     int minDays(vector<int>& bloomDay, int m, int k) {
-        int l = *min_element(bloomDay.begin(), bloomDay.end());
-        int r = *max_element(bloomDay.begin(), bloomDay.end());
-        int ans = -1;
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (check(mid, bloomDay, k, m)) {
-                ans = mid;
-                r = mid - 1;
-            } else
-                l = mid + 1;
+        int n=bloomDay.size();
+        int l=*min_element(bloomDay.begin(),bloomDay.end());
+        int r=*max_element(bloomDay.begin(),bloomDay.end());
+        int ans=-1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(check(mid,bloomDay,k,m)){
+                ans=mid;
+                r=mid-1;
+            }else{
+                l=mid+1;
+            }
         }
         return ans;
     }
