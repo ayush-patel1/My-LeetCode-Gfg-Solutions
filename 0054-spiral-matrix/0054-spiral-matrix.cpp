d78a401cb7f58ad36d7dc678aa276660ab27dac0
Ayush Patel <148226375+ayush-patel1@users.noreply.github.com>
2026-07-29 16:04:31 +0530

    Time: 0 ms (100%), Space: 9.4 MB (17.64%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,38 +1,39 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-      
-       int count=0;
-       int m=matrix.size(); //no. of rows 
-       int n=matrix[0].size();  //no. of cols
-       int left=0, right=n-1, top=0, bottom=m-1;
-       int total_ele=m*n;
-       vector<int> v;
-       while(count<total_ele){
-                 for(int i=left;i<=right && count<total_ele;i++){
-                    v.push_back(matrix[top][i]);
-                    count++;
-                 }
-                 top++;
+        int n = matrix.size();
+        int m = matrix[0].size();
 
-                  for(int i=top ;i<=bottom && count<total_ele;i++){
-                     v.push_back(matrix[i][right]);
-                     count++;
-                  }
-                  right--;
+        int top = 0, left = 0, right = m - 1, bot = n - 1;
+        int tot = n * m, cnt = 0;
+        vector<int> ans;
 
-                   for(int i=right;i>=left && count<total_ele;i--){
-                    v.push_back(matrix[bottom][i]);
-                    count++;
-                 }
-                 bottom--;
+        while (cnt < tot) {
+            for (int i = left; i <= right && cnt < tot; i++) {
+                ans.push_back(matrix[top][i]);
+                cnt++;
+            }
+            top++;
 
-                  for(int i=bottom;i>=top && count<total_ele;i--){
-                    v.push_back(matrix[i][left]);
-                    count++;
-                 }
-                 left++;
-       }
-       return v;
+            for (int i = top; i <= bot && cnt < tot; i++) {
+                ans.push_back(matrix[i][right]);
+                cnt++;
+            }
+            right--;
+
+            for (int i = right; i >= left && cnt < tot; i--) {
+                ans.push_back(matrix[bot][i]);
+                cnt++;
+            }
+            bot--;
+
+            for (int i = bot; i >= top && cnt < tot; i--) {
+                ans.push_back(matrix[i][left]);
+                cnt++;
+            }
+            left++;
+        }
+
+        return ans;
     }
 };
