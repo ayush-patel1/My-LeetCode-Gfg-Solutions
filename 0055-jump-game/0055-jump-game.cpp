4451f772c2c4309fd861f41ef5ceb04e66b5a205
Ayush Patel <148226375+ayush-patel1@users.noreply.github.com>
2026-08-02 00:46:06 +0530

    Time: 0 ms (100%), Space: 52.3 MB (83.8%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -4,9 +4,9 @@ public:
         int mx=0;
         int n=nums.size();
         for(int i=0;i<n;i++){
-            if(mx>=n-1) return true;
-            if(mx==i && nums[i]==0) return false;
+            if(mx<i) return false;
             mx=max(mx,nums[i]+i);
+            if(mx>=n-1) return true;
         }
         return false;
     }
