1b79aa344e186d16bb217812c0c2b1969da46f23
Ayush Patel <148226375+ayush-patel1@users.noreply.github.com>
2026-08-02 00:44:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int maxJump=nums[0];
-        for(int i=1;i<nums.size();i++){
-            if(i<=maxJump){
-                maxJump=max(maxJump,i+nums[i]);
-            }
-        }
-        if(maxJump>=nums.size()-1){
-            return true;
+        int mx=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(mx<i) return false;
+            mx=max(mx,nums[i]+i);
+            if(mx>=n) return true;
         }
         return false;
     }
