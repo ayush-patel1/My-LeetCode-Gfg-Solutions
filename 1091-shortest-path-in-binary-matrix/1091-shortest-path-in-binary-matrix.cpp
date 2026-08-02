651aad9a04dca14b6ecf686875f7982248b4a75e
Ayush Patel <148226375+ayush-patel1@users.noreply.github.com>
2026-08-02 16:30:13 +0530

    Time: 13 ms (78.22%), Space: 26.4 MB (26.48%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,32 +1,36 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        queue<pair<pair<int,int>,int>>q;
-        if(grid[0][0]==1)return-1;
-        if(grid[0][0]==0&&m==1&&n==1)return 1;
+        int n = grid.size();
+        int m = grid[0].size();
+        if (grid[0][0] == 1 || grid[n-1][m-1] == 1) return -1;
 
+        queue<pair<pair<int,int>,int>> q;
+        vector<vector<int>> vis(n, vector<int>(m, 0));
         q.push({{0,0},1});
-        vector<vector<int>>vis(n,vector<int>(m,0));
-        while(!q.empty()){
-        int x=q.front().first.first;
-        int  y=q.front().first.second;
-        int a=q.front().second;
-        q.pop();
-        for(int i=-1;i<=1;i++){
-            for(int j=-1;j<=1;j++){
-                int nr=x+i;
-                int nc=y+j;
-                if(nr>=0&&nc>=0&&nr<n&&nc<m&&vis[nr][nc]==0&&grid[nr][nc]==0){
-                    if(nr==n-1&&nc==m-1)return a+1;
-                    vis[nr][nc]=1;
-                    q.push({{nr,nc},a+1});
+        vis[0][0] = 1;
 
+        while (!q.empty()) {
+            auto node = q.front();
+            q.pop();
+            int r = node.first.first;
+            int c = node.first.second;
+            int step = node.second;
+
+            if (r == n-1 && c == m-1) return step;
+
+            for (int i=-1; i<=1; i++) {
+                for (int j=-1; j<=1; j++) {
+                    int nr = r+i;
+                    int nc = c+j;
+                    if (nr>=0 && nr<n && nc>=0 && nc<m && !vis[nr][nc] && grid[nr][nc]==0) {
+                        vis[nr][nc] = 1;
+                        q.push({{nr,nc}, step+1});
+                    }
                 }
             }
-        }
         }
         return -1;
     }
 };
+;
