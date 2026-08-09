e37ef66b471f8bbb4c55afa695f238f65b15e7e1
Ayush Patel <148226375+ayush-patel1@users.noreply.github.com>
2026-08-09 12:17:12 +0530

    Time: 177 ms (33.79%), Space: 225.8 MB (41.02%) - LeetHub

--- 4014-minimum-total-price-after-applying-discounts/4014-minimum-total-price-after-applying-discounts.cpp
+++ 4014-minimum-total-price-after-applying-discounts/4014-minimum-total-price-after-applying-discounts.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     double minPrice(vector<int>& prices, vector<int>& discounts) {
-        int n = prices.size();
-        int m = discounts.size();
-
-        sort(prices.rbegin(), prices.rend());
-        sort(discounts.rbegin(), discounts.rend());
-
-        double sum = 0;
-
-        for (int i = 0; i < min(n, m); i++) {
-            sum += prices[i] * (100.0 - discounts[i]) / 100.0;
+        int n=prices.size(); 
+        int m=discounts.size();
+        sort(prices.rbegin(),prices.rend());
+        sort(discounts.rbegin(),discounts.rend());
+        double sum=0;
+        for(int i=0;i<min(n,m);i++){
+           sum+=((100.0-discounts[i]*1.0)*prices[i]*1.0)/100.0;
         }
-
-        for (int i = m; i < n; i++) {
-            sum += prices[i];
+        if(n>min(n,m)){
+            for(int i=m;i<n;i++){
+               sum+=prices[i];
+            }
         }
-
         return sum;
     }
 };
