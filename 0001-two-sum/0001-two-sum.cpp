20367c84975c02f0481599651181cd64306a3296
Ayush Patel <148226375+ayush-patel1@users.noreply.github.com>
2026-08-13 16:28:50 +0530

    Time: 0 ms (100%), Space: 14.7 MB (57.71%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,17 +3,22 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n=nums.size();
         int l=0,r=n-1;
+    
         vector<pair<int,int>>v;
-        for(int i=0;i<n;i++) v.push_back({nums[i],i});
-        sort(v.begin(),v.end());
+        for(int i=0;i<n;i++){
+            v.push_back({nums[i],i});
+        }
+
+            sort(v.begin(),v.end());
         while(l<r){
-            int sm=v[l].first+v[r].first;
-            if(sm==target){
-               return {v[l].second,v[r].second};
-            }else if(sm<target){
-                l++;
-            }else r--;
+            int cur=v[l].first+v[r].first;
+            if(cur==target){
+                return {v[l].second,v[r].second};
+            }else if(cur>target){
+                r--;
+            }else l++;
         }
         return {-1,-1};
     }
+
 };
