0ca3d78f729417afb4e4e6a679dab98dc1c86cba
Ayush Patel <148226375+ayush-patel1@users.noreply.github.com>
2026-09-26 12:21:26 +0530

    Time: 66 ms (89.72%), Space: 122.8 MB (46.73%) - LeetHub

--- 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.cpp
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
     string evaluate(string s, vector<vector<string>>& knowledge) {
-        unordered_map<string,string>mp;
-        for(auto &k:knowledge){
-            mp[k[0]]=k[1];
+        unordered_map<string, string> mp;
+        for (auto& k : knowledge) {
+            mp[k[0]] = k[1];
         }
-        bool flg=false;
-        string ans="";
-        string temp="";
-        for(auto &ch:s){
-            if(ch==')'){
-                if(mp.count(temp)){
-                    ans+=mp[temp];
-                }else{
-                    ans+='?';        
+        bool flg = false;
+        string ans = "";
+        string temp = "";
+        for (auto& ch : s) {
+            if (ch == ')') {
+                if (mp.count(temp)) {
+                    ans += mp[temp];
+                } else {
+                    ans += '?';
                 }
-                temp="";
-                flg=false;
-                continue;
-            }
-            if(flg){
-               temp+=ch;
-               continue;
-            }
-            if(ch=='('){
-                 flg=true;
-                 continue;
-            }
-            ans+=ch;
+                temp = "";
+                flg = false;
+            } else if (flg) {
+                temp += ch;
+            } else if (ch == '(') {
+                flg = true;
+            } else
+                ans += ch;
         }
         return ans;
     }
